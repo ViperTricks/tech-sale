c0852bc6213ebd6cbe4a1a9fb207f6724268fff8
Vipertricks <hodactoan181905@gmail.com>
2026-04-24 01:49:16 +0700

    add env

--- src/api.js
+++ src/api.js
@@ -1,6 +1,3 @@
-const API =
-    window.location.hostname === "localhost"
-        ? "http://localhost:3000"
-        : "https://tech-sale-be.onrender.com";
+const API = import.meta.env.VITE_API_URL || "https://tech-sale-be.onrender.com";
 
 export default API;
