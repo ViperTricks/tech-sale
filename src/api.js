d57026a9a18db2d7394f474940933c012376b3e3
Vipertricks <hodactoan181905@gmail.com>
2026-04-24 01:04:21 +0700

    change port

--- src/api.js
+++ src/api.js
@@ -1,3 +1,6 @@
-const API = "http://localhost:3000";
+const API =
+    window.location.hostname === "localhost"
+        ? "http://localhost:3000"
+        : "https://tech-sale-be.onrender.com";
 
 export default API;
